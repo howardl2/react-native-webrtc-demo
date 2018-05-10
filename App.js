fc0e58e9878f3ae079855201d50d275e23f6b3f2
Howard Liu <howardliu94@gmail.com>
2018-05-09 17:52:27 -0700

    Working webRTC connection with kurento-bls

--- App.js
+++ App.js
@@ -22,185 +22,6 @@ import {
   getUserMedia,
 } from 'react-native-webrtc';
 
-
-let peerConnection;
-const url = 'wss://push.aws-stage-rt.veritone.com/socket';
-const connectionId = makeId(6);
-
-function makeId(length) {
-  // generates a random string as the connectionId
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < length; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
-};
-
-function getLocalStream(isFront, callback) {
-  // TLDR: makes the video appear
-  let videoSourceId;
-
-  // on android, you don't have to specify sourceId manually, just use facingMode
-  if (Platform.OS === 'ios') {
-    MediaStreamTrack.getSources(sourceInfos => {
-      console.log("sourceInfos: ", sourceInfos);
-
-      for (const i = 0; i < sourceInfos.length; i++) {
-        const sourceInfo = sourceInfos[i];
-        if(sourceInfo.kind == "video" && sourceInfo.facing == (isFront ? "front" : "back")) {
-          videoSourceId = sourceInfo.id;
-        }
-      }
-    });
-  }
-  getUserMedia({
-    audio: true,
-    video: {
-      mandatory: {
-        minWidth: 640, // Provide your own width, height and frame rate here
-        minHeight: 360,
-        minFrameRate: 30,
-      },
-      facingMode: (isFront ? "user" : "environment"),
-      optional: (videoSourceId ? [{sourceId: videoSourceId}] : []),
-    }
-  }, function (stream) {
-    console.log('getUserMedia success', stream);
-    callback(stream);
-  }, logError);
-};
-
-
-// function createPC(socketId, isOffer, localStream) {
-//   // TLDR: creates the WebRTC peer connection
-//   // TODO: pc functions we need:
-//   //    didGenerateOffer (probably createOffer)
-//   //    hasICECandidate (probably onICECandidate)
-//   const configuration = {};
-//   const pc = new RTCPeerConnection(configuration);
-//   pcPeers[socketId] = pc;
-
-//   pc.onicecandidate = function (event) {
-//     console.log('onicecandidate', event.candidate);
-//     // if (event.candidate) {
-//     //   socket.emit('exchange', {'to': socketId, 'candidate': event.candidate });
-//     // }
-//   };
-
-
-//   function createOffer(socketId) {
-//     pc.createOffer(function(desc) {
-//       console.log('createOffer', desc);
-//       pc.setLocalDescription(desc, function () {
-//         console.log('setLocalDescription', pc.localDescription);
-//         socket.emit('exchange', {'to': socketId, 'sdp': pc.localDescription });
-//       }, logError);
-//     }, logError);
-//   }
-
-//   pc.onnegotiationneeded = function () {
-//     console.log('onnegotiationneeded');
-//     if (isOffer) {
-//       createOffer(socketId);
-//     }
-//   }
-
-
-
-//   pc.oniceconnectionstatechange = function(event) {
-//     console.log('oniceconnectionstatechange', event.target.iceConnectionState);
-//     if (event.target.iceConnectionState === 'completed') {
-//       setTimeout(() => {
-//         getStats();
-//       }, 1000);
-//     }
-//     if (event.target.iceConnectionState === 'connected') {
-//       createDataChannel();
-//     }
-//   };
-//   pc.onsignalingstatechange = function(event) {
-//     console.log('onsignalingstatechange', event.target.signalingState);
-//   };
-
-
-//   pc.onaddstream = function (event) {
-//     console.log('onaddstream', event.stream);
-//     container.setState({info: 'One peer join!'});
-
-//     const remoteList = container.state.remoteList;
-//     remoteList[socketId] = event.stream.toURL();
-//     container.setState({ remoteList: remoteList });
-//   };
-//   pc.onremovestream = function (event) {
-//     console.log('onremovestream', event.stream);
-//   };
-
-//   pc.addStream(localStream);
-//   function createDataChannel() {
-//     if (pc.textDataChannel) {
-//       return;
-//     }
-//     const dataChannel = pc.createDataChannel("text");
-
-//     dataChannel.onerror = function (error) {
-//       console.log("dataChannel.onerror", error);
-//     };
-
-//     dataChannel.onmessage = function (event) {
-//       console.log("dataChannel.onmessage:", event.data);
-//       container.receiveTextData({user: socketId, message: event.data});
-//     };
-
-//     dataChannel.onopen = function () {
-//       console.log('dataChannel.onopen');
-//       container.setState({textRoomConnected: true});
-//     };
-
-//     dataChannel.onclose = function () {
-//       console.log("dataChannel.onclose");
-//     };
-
-//     pc.textDataChannel = dataChannel;
-//   }
-//   return pc;
-// }
-
-// function exchange(data) {
-//   const fromId = data.from;
-//   let pc;
-//   if (fromId in pcPeers) {
-//     pc = pcPeers[fromId];
-//   } else {
-//     pc = createPC(fromId, false);
-//   }
-
-//   if (data.sdp) {
-//     console.log('exchange sdp', data);
-//     pc.setRemoteDescription(new RTCSessionDescription(data.sdp), function () {
-//       if (pc.remoteDescription.type == "offer")
-//         pc.createAnswer(function(desc) {
-//           console.log('createAnswer', desc);
-//           pc.setLocalDescription(desc, function () {
-//             console.log('setLocalDescription', pc.localDescription);
-//             socket.emit('exchange', {'to': fromId, 'sdp': pc.localDescription });
-//           }, logError);
-//         }, logError);
-//     }, logError);
-//   } else {
-//     console.log('exchange candidate', data);
-//     // TODO: this line should be after on record
-//     // pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-//   }
-// }
-
-
-function logError(error) {
-  console.log("logError", error);
-};
-
-
 function getStats() {
   const pc = pcPeers[Object.keys(pcPeers)[0]];
   if (pc.getRemoteStreams()[0] && pc.getRemoteStreams()[0].getAudioTracks()[0]) {
@@ -212,166 +33,251 @@ function getStats() {
   }
 }
 
-
-function createPeerConnection(localStream) {
-  const configuration = {};
-  const pc = new RTCPeerConnection(configuration);
-  // pc.createOffer
-  // pcPeers[connectionId] = pc;
-  peerConnection = pc;
-  peerConnection.addStream(localStream);
-
-};
-
-
-function setupWebRTCSession(component) {
-  getLocalStream(component.state.isFront, (stream) => {
-    // localStream = stream;
-    // should see video feed on screen after this
-    component.setState({
-      videoURL: stream.toURL(),
-      localStream: stream
-    });
-
-    createPeerConnection(stream);
-
-  });
-};
-
-
-const socket = new WebSocket(url);
-socket.onopen = (event) => {
-  console.log('open', event);
-  peerConnection.createOffer(connectionId).then((offer) => {
-    console.log('settinglocaldescription:', offer);
-    const params = {
-      "id": "start", 
-      "sdpOffer": offer.sdp,
-      "jobId": "",
-      "sourceId": "",
-      "authToken": ""
-    };
-    console.log('sending to server:', params);
-    socket.send(JSON.stringify(params));
-    return peerConnection.setLocalDescription(offer);
-  })
-  .then(() => {
-    console.log('send to server');
-  })
-  .catch((error) => {
-    console.log('error:', error);
-  });
-};
-
-socket.onerror = (error) => {
-  console.log('error:', error);
-
-  //TODO: stop recording
-};
-
-socket.onclose = (event) => {
-  // const viewIndex = peerConnection.viewIndex;
-  peerConnection.close();
-  peerConnection = null;
-  //TODO: stop recording
-};
-
-socket.onmessage = (event) => {
-  console.log('message', event);
-  console.log(event.data);
-};
-
-
-
-
-
 export default class App extends Component {
+  static defaultProps = {
+    socketURL: 'wss://push.aws-dev-rt.veritone.com/socket',
+    jobId: '36039',
+    sourceId: "Howard Dev Test",
+    token: '39a47243-5c30-47bb-800d-de5d7328c827',
+  };
+
   state = {
-    isFront: false,
+    isFront: true,
     videoURL: null,
     localStream: null,
     record: 'Start Recording',
-    socket: null
+    socket: null,
+    peerConnection: null,
+    connectionId: '',
+    configuration: {},
   };
 
   componentWillMount() {
     // setup the local stream (phone's camera)
-    setupWebRTCSession(this);
+    const connectionId = this._makeId(6);
+    const pc = new RTCPeerConnection(this.state.configuration);
     
-    // createPC(connectionId, true, this.state.localStream);
-    
+    this.setState({
+      peerConnection: pc,
+      connectionId: connectionId
+    });
+
+    this.getLocalStream().then((stream) => {
+      this.setState({
+        videoURL: stream.toURL(),
+        localStream: stream
+      });
+  
+      this.state.peerConnection.addStream(stream);
+      
+      this.state.peerConnection.onicecandidate = (event) => {
+        if (event && event.candidate) {
+          const params = {
+            "id": "onIceCandidate",
+            "candidate": {
+              "candidate": event.candidate.candidate,
+              "sdpMid": event.candidate.sdpMid,
+              "sdpMLineIndex": event.candidate.sdpMLineIndex
+            }
+          }
+          console.log('WebRTC: sending onIceCandidate:', JSON.stringify(params));
+          this.state.socket.send(JSON.stringify(params));
+        }
+      };
+    });
   };
 
   componentWillUnmount() {
-    peerConnection.close();
-    peerConnection = null;
-    // pcPeers[connectionId].close();
-    // delete pcPeers[connectionId];
+    this.state.peerConnection.close();
+    this.setState({
+      peerConnection: null,
+      record: (this.state.record === 'Start Recording') ? 'Stop Recording' : 'Start Recording',
+    });
     this.state.localStream.release();
-    //socket.close();
   };
 
-
-  setupWebSocket = () => {
-    const socket = new WebSocket(url);
-    socket.onopen = (event) => {
-      console.log('open', event);
-      peerConnection.createOffer(connectionId);
-    };
-
-    socket.onerror = (error) => {
-      console.log('error:', error);
-
-      //TODO: stop recording
-    };
-
-    socket.onclose = (event) => {
-      // const viewIndex = peerConnection.viewIndex;
-      peerConnection.close();
-      peerConnection = null;
-      //TODO: stop recording
-    };
-
-    socket.onmessage = (event) => {
-      console.log('message', event);
-    };
-
-    this.setState({
-      socket: socket
+  getLocalStream = () => {
+    return MediaStreamTrack.getSources().then((sourceInfos) => {
+      console.log('Source Infos:', sourceInfos);
+      let videoSourceId;
+      for (let i = 0; i < sourceInfos.length; i++) {
+        const sourceInfo = sourceInfos[i];
+        if (sourceInfo.kind == 'video' && sourceInfo.facing == (this.state.isFront ? 'front' : 'back')) {
+          videoSourceId = sourceInfo.id;
+        }
+      }
+      return getUserMedia({
+        audio: true,
+        video: {
+          mandatory: {
+            minWidth: 500,
+            minHeight: 300,
+            minFrameRate: 30
+          },
+          facingMode: (this.state.isFront ? 'user' : 'environment'),
+          optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
+        }
+      });
     });
   };
 
 
+  setupWebSocket = () => {
+    const socket = new WebSocket(this.props.socketURL);
+    socket.onopen = (event) => {
+      console.log('WebSocket: opened:', event);
+      this._createOffer();
+      console.log(this.state.peerConnection._localStreams);
+      
+    };
+  
+    socket.onerror = (error) => {
+      console.log('WebSocket: error:', error);
+      this.state.peerConnection.close();
+      this.setState({
+        peerConnection: null,
+        record: (this.state.record === 'Start Recording') ? 'Stop Recording' : 'Start Recording',
+      });
+    };
+  
+    socket.onclose = (event) => {
+      // const viewIndex = peerConnection.viewIndex;
+      this.state.peerConnection.close();
+      this.setState({
+        peerConnection: null,
+        record: (this.state.record === 'Start Recording') ? 'Stop Recording' : 'Start Recording',
+      });
+    };
+  
+    socket.onmessage = (event) => {
+      let message;
+      if (event && event.data) {
+        message = JSON.parse(event.data);
+      } else {
+        console.warn('Websocket returned an empty message.');
+        return;
+      }
+      switch (message.id) {
+        case 'startResponse': {
+          console.log('startResponse\n', message);
+          if (message && message.sdpAnswer) {
+            const sdpAnswer = message.sdpAnswer;
 
+            var description = new RTCSessionDescription(message.sdpAnswer);
+            description.type = 'answer';
+            description.sdp = message.sdpAnswer;
 
+            console.log('description:', description);
+            console.log('WebRTC: before remote description state:', this.state.peerConnection.signalingState);
+            this.state.peerConnection.setRemoteDescription(description).then(() => {
+              console.log('Remote description set: state:', this.state.peerConnection);
+              
+            })
+            .catch((error) => {
+              console.warn('Remote Description ERROR:', error);
+            });
+          } else {
+            console.warn('Start Response did not contain an sdpAnswer');
+          }
+          break;
+        };
+        case 'iceCandidate': {
+          if (message && message.candidate) {
+            const candidate = new RTCIceCandidate(candidate=message.candidate);
+            console.log('WebRTC: adding iceCandidate:', candidate);
+            this.state.peerConnection.addIceCandidate(candidate).then(() => {
+              console.log('WebRTC: Successfully added iceCandidate to client: iceConnectionState:', this.state.peerConnection.iceConnectionState);
+            }).catch((error) => {
+              console.warn('WebRTC: Error adding iceCandidate:', error);
+            });
+          }
+          break;
+        };
+        case 'chunk': {
+          console.log('chunk', message);
+          break;
+        };
+        case 'status': {
+          console.log('status', message.chunk);
+          console.log('component state:', this.state.peerConnection);
+          break;
+        };
+        case 'error': {
+          console.error('WEBSOCKET SERVER ERROR:', message);
+          this.state.peerConnection.close();
+          this.setState({
+            peerConnection: null,
+            record: (this.state.record === 'Start Recording') ? 'Stop Recording' : 'Start Recording',
+          });
+          break;
+        };
+        default: {
+          console.warning('Unrecognized message from the web socket');
+        }
+      }
+    };
+    this.setState({socket});
+  };
 
+  _createOffer = () => {
+    this.state.peerConnection.createOffer().then((offer) => {
+      console.log('WebRTC: settinglocaldescription:', offer);
+      this.state.peerConnection.setLocalDescription(offer).then(() => {
+        console.log('WebRTC: local description set');
+        // console.log('WebRTC: state:', this.state.peerConnection.signalingState);
+        const params = {
+          "id": "start", 
+          "sdpOffer": offer.sdp,
+          "jobId": this.props.jobId,
+          "sourceId": this.props.sourceId,
+          "authToken": this.props.token
+        };
+        console.log('WebSocket: sending to server:', params);
+        this.state.socket.send(JSON.stringify(params));
+      });
+  
+    }).then(() => {
+      console.log('WebRTC: send to server');
+    }).catch((error) => {
+      console.error('WebRTC: error:', error);
+    });
+  };
+
+  _makeId = (length) => {
+    // generates a random string as the connectionId
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < length; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+  };
 
   _switchVideoType = () => {
     const isFront = !this.state.isFront;
     this.setState({isFront});
-    getLocalStream(isFront, (stream) => {
+    this._getLocalStream().then((stream) => {
       if (this.state.localStream) {
-        for (const id in pcPeers) {
-          const pc = pcPeers[id];
-          pc && pc.removeStream(localStream);
-        }
+        this.state.peerConnection.removeStream(this.state.localStream);
         this.state.localStream.release();
       }
-      // localStream = stream;
+      
       this.setState({
         videoURL: stream.toURL(),
         localStream: stream
       });
 
-      for (const id in pcPeers) {
-        const pc = pcPeers[id];
-        pc && pc.addStream(this.state.localStream);
-      }
+      this.state.peerConnection.addStream(stream);
     });
   };
 
   _startRecording = () => {
-    // setupWebSocket(connectionId, this.state.localStream);
+    if (this.state.record === 'Start Recording') {
+      this.setupWebSocket();
+    } else {
+      this.state.socket.close();
+    }
     this.setState({
       record: (this.state.record === 'Start Recording') ? 'Stop Recording' : 'Start Recording'
     });
